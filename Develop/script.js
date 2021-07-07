aaf621f5653c0637ace0f771d757706e66ff6041
Abby Roberg <abbyroberg@gmail.com>
2021-07-06 19:34:08 -0500

    psuedocode requirements

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,15 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Generate random password
+var generatePassword = function() {
+  // Prompt for password length
+    // Validate response between 8 and 128 characters
+  // Prompt for character types to include
+    //  Validate at least 1 character type selected
+  // Create password based on inputs
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
